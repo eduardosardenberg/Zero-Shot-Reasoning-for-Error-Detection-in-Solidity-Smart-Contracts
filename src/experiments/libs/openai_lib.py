0f51354c671ddde7a3ae3dd646c614c2d14b23b6
Eduardo Sardenberg <eduardo.sardenberg99@gmail.com>
2026-02-17 00:29:06 -0300

    Atualizando exp 2

--- src/experiments/libs/openai_lib.py
+++ src/experiments/libs/openai_lib.py
@@ -12,17 +12,31 @@ def call_openai(model_name, prompt, max_attempts=1, timeout=20):
     attempt = 0
     while attempt < max_attempts:
         try:
+            #Para gpt 5 ou superior
+            # response = client.responses.create(
+            #     model=model_name,
+            #     input=prompt,
+            #     reasoning={"effort": "minimal"},
+            #     #temperature=0.0,
+            #     #seed=64,
+            #     timeout=timeout
+            # )
+
+            #return response.output_text.strip()
+
             completion = client.chat.completions.create(
                 model=model_name,
                 messages=[
                     {"role": "user", "content": prompt}
                 ],
                 response_format = { "type": "json_object" },
-                #temperature = 0.0,
+                temperature = 0.0,
                 seed = 64,
                 timeout = timeout,
             )
+
             return completion.choices[0].message.content.strip()
+        
         except Exception as e:
             print(f"Attempt {attempt+1} failed: {e}")
             attempt += 1
